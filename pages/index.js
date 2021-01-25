bc0f50886684347ce3fb8d488c6e68226ed9ecde
Victor Premoli <victor.premoli57@gmail.com>
2021-01-25 20:39:43 -0300

    Inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from "../db.json"
+import Widget from "../src/components/Widget"
+import Footer from "../src/components/Footer"
+import GitHubCorner from "../src/components/GitHubCorner"
+import QuizBackground from "../src/components/QuizBackground"
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   flex:1;
+//   background-size: cover;
+//   background-position: center;
+//   background-image: url(${db.bg});
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg} >
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>The legend of zelda</h1>
+            </Widget.Header>
+
+            <Widget.Content>
+              <p> loremipsum dolor sit ameet.... </p>
+            </Widget.Content>
+          </Widget>
+
+          <Widget>
+            <Widget.Header>
+              <h1>The legend of zelda</h1>
+            </Widget.Header>
+
+            <Widget.Content>
+              <p> loremipsum dolor sit ameet.... </p>
+            </Widget.Content>
+          </Widget>
+          <Footer/>
+        </QuizContainer>
+        <GitHubCorner projectUrl='https://github.com/victorpremoli' />
+    </QuizBackground>
+  );
 }
+ 
